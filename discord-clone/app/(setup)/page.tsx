e806cddc6e0a69329a921958d195b9a7969dd7b3
1Dire <d01038145300@nate.com>
2023-11-14 03:37:43 +0900

    initial

--- discord-clone/app/(setup)/page.tsx
+++ discord-clone/app/(setup)/page.tsx
@@ -1,6 +1,8 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModel } from "@/components/modals/initial-modal";
+
 const SetupPage = async () => {
     const profile = await initialProfile();
 
@@ -18,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
       }
     
-  return <div>create a server</div>;
+  return <InitialModel></InitialModel>;
 };
 
 export default SetupPage;
