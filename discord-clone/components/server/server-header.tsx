e7d3c3bc56558c65c77c81738ae78544a1d996eb
1Dire <d01038145300@nate.com>
2023-11-20 07:21:56 +0900

    서버 초대 링크 새로고침

--- discord-clone/components/server/server-header.tsx
+++ discord-clone/components/server/server-header.tsx
@@ -3,14 +3,6 @@
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useModal } from "@/hooks/use-modal-store";
-import {
   ChevronDown,
   LogOut,
   PlusCircle,
@@ -20,12 +12,23 @@ import {
   Users,
 } from "lucide-react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
+
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -40,7 +43,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
           {isModerator && (
-            <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+            <DropdownMenuItem
+              onClick={() => onOpen("invite", { server })}
+              className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+            >
               Invite People
               <UserPlus className="h-4 w-4 ml-auto" />
             </DropdownMenuItem>
