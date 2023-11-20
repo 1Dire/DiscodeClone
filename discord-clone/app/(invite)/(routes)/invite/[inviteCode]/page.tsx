d3095d01a4ee32d43d922a4f0c07dc951a1c47b8
1Dire <d01038145300@nate.com>
2023-11-21 02:01:01 +0900

    멤버 관리 기능 구현

--- discord-clone/app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ discord-clone/app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,35 +1,63 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
 
 interface InviteCodePageProps {
   params: {
     inviteCode: string;
   };
-}
-const inviteCodePage = async ({ params }: InviteCodePageProps) => {
+};
+
+const InviteCodePage = async ({
+  params
+}: InviteCodePageProps) => {
   const profile = await currentProfile();
+
   if (!profile) {
     return redirectToSignIn();
   }
+
   if (!params.inviteCode) {
     return redirect("/");
   }
+
   const existingServer = await db.server.findFirst({
     where: {
       inviteCode: params.inviteCode,
       members: {
         some: {
-          profileId: profile.id,
-        },
-      },
-    },
+          profileId: profile.id
+        }
+      }
+    }
   });
+
   if (existingServer) {
     return redirect(`/servers/${existingServer.id}`);
   }
 
-  return <div>hello</div>;
-};
-export default InviteCodePageProps;
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.inviteCode,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          }
+        ]
+      }
+    }
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  
+  return null;
+}
+ 
+export default InviteCodePage;
